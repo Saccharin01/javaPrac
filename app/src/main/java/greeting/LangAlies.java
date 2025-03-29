a9e390b11df15d3859eca1a5106b21922380fbb8
saccharin01 <chousik01@gmail.com>
2025-03-29 18:29:17 +0900

    comment | LangAlies.java
    enum 테이블 class 에 DocString 추가

--- app/src/main/java/greeting/LangAlies.java
+++ app/src/main/java/greeting/LangAlies.java
@@ -1,7 +1,14 @@
 package greeting;
 
 
-//
+/**
+ * 사용자의 입력값을 검증하기 위한 enum 테이블을 생성합니다.
+ * 해당 테이블은 korea, english, japan 이라는 문자열 입력에 대해 ko, en, jp 라는 문자열을 반환합니다.
+ * 해당 클래스를 작성한 이유는 KoRea 처럼 대소문자를 섞는 케이스에 대한 대응보다 문자열의 오타에 대한 대응에 중점을 둡니다.
+ * 예를 들어, kreoa, engilsh 처럼 문자열 입력에 오타가 섞여있을 경우, 해당 부분에 대한 방어를 도모합니다.
+ */
+
+
 public enum LangAlies {
     KOREA("ko"),
     ENGLISH("en"),
