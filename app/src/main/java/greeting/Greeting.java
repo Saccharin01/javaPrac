d467b3b0391e8a46f2448dc3e5114ec899560e9d
saccharin01 <chousik01@gmail.com>
2025-03-29 19:26:10 +0900

    feat | Greeting.java
    normalize 메서드는 사용자의 입력값을 검사하는 메서드인데, 해당 메서드를 enum 클래스로 분리했기 떄문에
    해당 부분을 삭제합니다.
    또한 인스턴스 내부의 불필요한 필드도 삭제합니다.

--- app/src/main/java/greeting/Greeting.java
+++ app/src/main/java/greeting/Greeting.java
@@ -1,42 +1,28 @@
 package greeting;
 import java.util.Map;
 
+import greeting.LangAlies;
+
+
 public class Greeting {
+
     private static final Map<String, String> translateTable = Map.of(
             "ko", "안녕하세요",
             "en", "hello",
             "jp", "こんにちは"
     );
 
-    private static final Map<String, String> aliesArgument = Map.of(
-            "korea", "ko",
-            "english", "en",
-            "japan", "jp"
-    );
-
     private final String lang;
 
     public Greeting(String lang) {
-        this.lang = normalizeInput(lang.toLowerCase());
+        this.lang = LangAlies.enumCheck(lang.toLowerCase());
         hello();
     }
 
-    private String normalizeInput(String input){
-        if(translateTable.containsKey(input)){
-            return input;
-        } else if (aliesArgument.containsKey(input)){
-            return aliesArgument.get(input);
-        } else {
-            return null;
-        }
-    }
-
     private void hello(){
-        if(lang != null){
-            System.out.println(translateTable.get(lang));
-        } else {
-            System.out.println("지원되지 않는 언어입니다.");
-        }
+        System.out.println(
+                translateTable.getOrDefault(this.lang, "지원되지 않는 언어입니다.")
+        );
     }
 
 }
