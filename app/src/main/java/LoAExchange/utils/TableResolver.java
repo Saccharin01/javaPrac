685e38f24c68f57c3b738dd018eda542ebbe623f
saccharin01 <chousik01@gmail.com>
2025-03-30 19:24:13 +0900

    new | TableResolver.jave
    생성된 테이블 값을 이용해 비즈니스 로직을 수행하는 모듈.
    배열 내에 인스턴스의 label, alies 값을 이용해 사용자의 입력값을 검사하며, 해당하는 값이 없을 경우
    매개변수 2인자로 전달되는 기본값 문자열을 반환

---
+++ app/src/main/java/LoAExchange/utils/TableResolver.java
@@ -0,0 +1,30 @@
+package LoAExchange.utils;
+
+import LoAExchange.LookupTable.LookupTable;
+import java.util.List;
+
+
+public class TableResolver {
+    private final List<LookupTable> table;
+    private final String defaultValue;
+
+    public TableResolver(List<LookupTable> table, String defaultValue) {
+        this.table = table;
+        this.defaultValue = defaultValue;
+    }
+
+    public String Resolve(String input){
+        if(input == null || input.isEmpty()) {
+            return this.defaultValue;
+        }
+
+        for(LookupTable table : table) {
+            if(table.getLabel().equalsIgnoreCase(input)
+                ||table.getAlies().equalsIgnoreCase(input)) {
+                return table.getLabel();
+            }
+        }
+        return this.defaultValue;
+    }
+
+}
