e9cd4158c33b21a0d9bb1286e8bbd34fcf54864c
saccharin01 <chousik01@gmail.com>
2025-03-30 23:17:56 +0900

    feat | APIRequestBody.java
    API 서버에 요청을 보내는 body를 생성하는 클래스로 리팩터링 합니다.
    
    해당 클래스는 DTO에서 정의한 requestBody 형식을 이용해 post 요청에 사용될 body를 구축합니다.

--- app/src/main/java/LoAExchange/APIRequestBody.java
+++ app/src/main/java/LoAExchange/APIRequestBody.java
@@ -1,49 +1,52 @@
 package LoAExchange;
 
 import LoAExchange.LookupTable.LookupTable;
-import LoAExchange.utils.JsonLoader;
+import LoAExchange.dto.RequestPayload;
 import LoAExchange.utils.TableResolver;
-import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.List;
 
 public class APIRequestBody {
 
-    private final String API_KEY;
-    private final String REQUEST_URL;
     private final TableResolver itemGradeResolve;
     private final TableResolver charClassResolve;
 
-    public APIRequestBody() {
-        Dotenv dotenv = Dotenv.load();
-        this.API_KEY = dotenv.get("API_KEY");
-        this.REQUEST_URL = dotenv.get("REQUEST_URL");
-
-        List<LookupTable> ItemGrade = JsonLoader.loadLookupTable("java/LoAExchange/data/ItemGrade.json");
-        List<LookupTable> CharClass = JsonLoader.loadLookupTable("java/LoAExchange/data/CharacterClass.json");
-
-
-        this.itemGradeResolve = new TableResolver(ItemGrade, "희귀");
-        this.charClassResolve = new TableResolver(CharClass, "건슬");
+    public APIRequestBody(TableResolver gradeTable, TableResolver classTable) {
+        this.itemGradeResolve = gradeTable;
+        this.charClassResolve = classTable;
     }
 
     public String resolveItemGrade(String input) {
-        return this.itemGradeResolve.Resolve(input);
+        return itemGradeResolve.Resolve(input);
     }
 
     public String resolveCharClass(String input) {
-        return this.charClassResolve.Resolve(input);
+        return charClassResolve.Resolve(input);
     }
 
-    public String getAPI_KEY(){
-        return this.API_KEY;
-    }
-    public String getREQUEST_URL(){
-        return this.REQUEST_URL;
+    /**
+     * payload 빌드 메서드입니다. 매개변수로 전달된 값을 활용해 DTO에서 정의한 바디를 빌드합니다.
+     * @param itemName : 아이템 이름입니다. 빈 값으로 둘 경우 아이템 코드에 해당하는 모든 아이템을 검색합니다.
+     * @param inputGrade : 아이템 등급입니다. 아이템 등급에 해당하는 문자열 값을 넣어야 합니다.
+     * @param inputClass : 캐릭터 클래스입니다. 해당하는 문자열을 넣어야 합니다.
+     * @return : DTO 에서 정의된 body를 반환합니다.
+     */
+    public RequestPayload createRequestPayload(String itemName, String inputGrade, String inputClass) {
+        return RequestPayload.builder()
+                .itemName(itemName)
+                .itemGrade(resolveItemGrade(inputGrade))
+                .characterClass(resolveCharClass(inputClass))
+                .build();
     }
 
-    public String buildRequestBody(){
+    /** createRequestPayload 메서드의 오버로딩 메서드입니다. DTO에서 아이템 이름은 기본값으로 지정되어 있기 때문에
+     * 해당 메서드를 이용해 아이템 이름에 대한 선택적 매개변수를 도모합니다.
+     */
 
-    return null;}
+    public RequestPayload createRequestPayload(String grade, String charClass) {
+        return createRequestPayload("", grade, charClass);
+    }
+
+
 
 }
