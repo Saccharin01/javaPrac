35f544eaa03243fc45ff6e42503fa73f1bb13e1d
saccharin01 <chousik01@gmail.com>
2025-03-31 12:26:42 +0900

    feat | DataBaseConnection.java
    dotEnv 로 처리한 url을 엔드포인트까지 묶은 한 블럭으로 처리하는 부분을 수정

--- app/src/main/java/LoAExchange/database/DataBaseConnection.java
+++ app/src/main/java/LoAExchange/database/DataBaseConnection.java
@@ -10,9 +10,14 @@ public class DataBaseConnection {
 
     private static final Dotenv dotenv = Dotenv.load();
 
-    private static final String URL = dotenv.get("DATABASE_URL");
+    private static final String HOST = dotenv.get("DATABASE_URL");
+    private static final String DB_NAME = dotenv.get("DATABASE_NAME");
     private static final String USER = dotenv.get("DATABASE_USERNAME");
     private static final String PASSWORD = dotenv.get("DATABASE_PASSWORD");
+
+    private static final String URL = """
+        jdbc:mariadb://%s/%s
+        """.formatted(HOST, DB_NAME);
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
